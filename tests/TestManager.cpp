5e6ccc1b9bfea9dfa097f64a8e169f77c890208b
Lars Dammann <larstd90@gmail.com>
2022-05-31 23:35:24 +0200

    More tests

--- tests/TestManager.cpp
+++ tests/TestManager.cpp
@@ -42,7 +42,7 @@ TEST(SingleFeatureManagerTests, CannotFreeBeforeAllocate) {
 	EXPECT_EQ(manager.Available("FOO"), 1);
 }
 
-TEST(SingleFeatureManagerTests, CannotAllocateTooMuch) {
+TEST(SingleFeatureManagerTests, CannotAllocateTooMuch1) {
 	Manager manager;
 	ASSERT_TRUE(manager.Add("FOO"));
 	ASSERT_EQ(manager.Available("FOO"), 1);
@@ -55,4 +55,40 @@ TEST(SingleFeatureManagerTests, CannotAllocateTooMuch) {
 	EXPECT_EQ(manager.Available("FOO"), 0);
 }
 
+TEST(SingleFeatureManagerTests, CannotAllocateTooMuch2) {
+	Manager manager;
+	ASSERT_TRUE(manager.Add("FOO", 2));
+	ASSERT_EQ(manager.Available("FOO"), 2);
+	// Cannot allocate more than we have
+	EXPECT_FALSE(manager.Allocate("FOO", 3));
+	// State not altered
+	EXPECT_EQ(manager.Available("FOO"), 2);
+}
 
+TEST(SingleFeatureManagerTests, CannotFreeTooMuch) {
+	Manager manager;
+	ASSERT_TRUE(manager.Add("FOO", 4));
+	ASSERT_EQ(manager.Available("FOO"), 4);
+	// Allocate a few
+	EXPECT_TRUE(manager.Allocate("FOO", 3));
+	EXPECT_EQ(manager.Available("FOO"), 1);
+	// Cannot free more than is allocated
+	EXPECT_FALSE(manager.Free("FOO", 4));
+	// State not altered
+	EXPECT_EQ(manager.Available("FOO"), 1);
+}
+
+TEST(SingleFeatureManagerTests, PartialFree) {
+	Manager manager;
+	ASSERT_TRUE(manager.Add("FOO", 4));
+	ASSERT_EQ(manager.Available("FOO"), 4);
+	// Allocate a few
+	EXPECT_TRUE(manager.Allocate("FOO", 3));
+	EXPECT_EQ(manager.Available("FOO"), 1);
+	// Free some but not all
+	EXPECT_TRUE(manager.Free("FOO", 2));
+	EXPECT_EQ(manager.Available("FOO"), 3);
+	// Free the rest
+	EXPECT_TRUE(manager.Free("FOO", 1));
+	EXPECT_EQ(manager.Available("FOO"), 4);
+}
